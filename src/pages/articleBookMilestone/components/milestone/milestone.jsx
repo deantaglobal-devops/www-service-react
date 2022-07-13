391f53fa5b48f7eb91f11e40e632ae98ec7a1fb9
Macuser 01 <macuser01@COSMCLTCHOI01s-MacBook-Pro.local>
2022-07-13 14:45:30 +0530

    LV-2683 - Task - Added post api for finalized manuscript of TOC

--- src/pages/articleBookMilestone/components/milestone/milestone.jsx
+++ src/pages/articleBookMilestone/components/milestone/milestone.jsx
@@ -209,6 +209,7 @@ export default function Milestone({
 
   const changeTaskStatus = async (_action, _taskId, _status) => {
     setIsLoading(true);
+    let isCompleted = false;
     await api
       .post("/task/change/status", {
         action: _action,
@@ -291,12 +292,22 @@ export default function Milestone({
             updateCurrentTaskCol(milestoneId, activeTaskId);
           }
         }
-
+        isCompleted = true;
         // toggleMilestone(milestoneId);
         getMilestoneData(milestoneId);
       })
       .catch((err) => console.log(err));
     setIsLoading(false);
+    if(isCompleted && _action === 'finish') {
+      api.post("task/ce/level/assess", {
+        taskId: _taskId,
+        projectId: projectData.projectId,
+        chapterId:0 
+      }).then((response) => {
+        console.log(response);
+      })
+      .catch((err) => console.log(err));
+    }
   };
 
   const confirmReject = (_taskId) => {
