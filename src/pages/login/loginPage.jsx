4bc8000ec4e40a6ead01bed6159f2dff30b84b95
koliveiradeanta <82088986+koliveiradeanta@users.noreply.github.com>
2022-07-14 15:13:00 +0530

    test pipeline

--- src/pages/login/loginPage.jsx
+++ src/pages/login/loginPage.jsx
@@ -9,7 +9,7 @@ import "./styles/LoginPage.styles.css";
 
 export function LoginPage() {
   useEffect(() => {
-    document.title = "Lanstad — Login";
+    document.title = "test Lanstad — Login";
   }, []);
 
   return (
