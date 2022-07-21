3f75d847788477a81c6555f7e3220ca5a90e3a8b
Vinícius Grippe <vinicius.grippe@deantaglobal.com>
2022-07-21 12:10:59 -0400

    Revert "LV-2767 - Fix: Changes tooltip's position in notification"
    This reverts commit ec9d251dbb4d976d80b03b3d1de20cabdb5927d3.

--- src/pages/notificationCenter/components/menuTabLanUI/menuTabLanUI.jsx
+++ src/pages/notificationCenter/components/menuTabLanUI/menuTabLanUI.jsx
@@ -1,16 +1,15 @@
-import React from "react";
-import { Tooltip } from "../../../../components/tooltip/tooltip";
-import "./styles/menuTabLanUI.styles.css";
+import React from 'react';
+import './styles/menuTabLanUI.styles.css';
 
 export default function MenuTabLanUI({ ...props }) {
   function onClick() {
-    if (typeof props.onClick !== "function") {
+    if (typeof props.onClick !== 'function') {
       console.error(
         `You need to pass a function as a Prop.
 					eg. () => myFunction()
 
 					eg. <MenuTab onClick={() => myFunction()}/>
-				`,
+				`
       );
       return;
     }
@@ -22,16 +21,10 @@ export default function MenuTabLanUI({ ...props }) {
     <button
       onClick={() => onClick()}
       className={`undefined ${
-        props.active ? "icon-text-bold active" : "icon-text"
+        props.active ? `icon-text-bold active` : 'icon-text'
       } MenuTab_tab`}
     >
-      {props.columnNumber !== 2 ? (
-        <Tooltip content={props.titleProject} direction="bottom max-wdth">
-          <p>{props.text}</p>
-        </Tooltip>
-      ) : (
-        <p>{props.text}</p>
-      )}
+      <span>{props.text}</span>
 
       {props.secondColumn && (
         <span className="second_column">{props.secondColumn}</span>
